a36cccc74f946b798643ebb0f0832cf083a3ff2f
Olga <olya.sereda.97@mail.ru>
2024-05-20 21:39:26 +0300

    update styles

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const progressBar = document.getElementById('progress__bar')
 
 function changeInputValue(val) {
-    // progressBar.style['background'] = `conic-gradient(#005CFF ${val}%, #EAF0F6 0);`
-    // progressBar.style.setProperty('background', `conic-gradient(#005CFF ${val}%, #EAF0F6 0);`)
-    // progressBar.style.background = `conic-gradient(#005CFF ${val}%, #EAF0F6 0);`
-
     const style = `
     background:
         radial-gradient(closest-side, white 79%, transparent 80% 100%),
@@ -14,23 +10,14 @@ function changeInputValue(val) {
 }
 
 
-function hide() {
-    const hideInput = document.getElementById("hide__input");
-
-    if (hideInput.checked === true) {
-        progressBar.classList.add('hide')
-    } else {
-        progressBar.classList.remove('hide')
-    }
+function hideCheckboxClick(element) {
+    element.checked
+        ? progressBar.classList.add('hide')
+        : progressBar.classList.remove('hide')
 }
 
-
-function animateInp() {
-    const animateInput = document.getElementById("animate__input");
-
-    if (animateInput.checked === true) {
-        progressBar.classList.add('rotation')
-    } else {
-        progressBar.classList.remove('rotation')
-    }
+function animateCheckboxClick(element) {
+    element.checked
+        ? progressBar.classList.add('rotation')
+        : progressBar.classList.remove('rotation')
 }
